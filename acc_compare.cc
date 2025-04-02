ca7e234c1408ffe3182dc0942be6a3c48eb2a3ae
huntermBerkeley <hunterm@berkeley.edu>
2025-04-02 10:55:36 -0600

    acc_bench

--- acc_compare.cc
+++ acc_compare.cc
@@ -26,6 +26,9 @@ double run_tile_experiment(std::string filename, CoOrdinate l_dims, CoOrdinate r
 
   //start pipe.
   int pipefd[2];
+  if (pipe(pipefd) == -1){
+    std::cerr << "Pipe creation failed\n";
+  }
   if (fork() == 0) {
    close(pipefd[0]);
    Tensor<double> delicious4d = Tensor<double>(prefix + filename, true);
@@ -46,7 +49,7 @@ double run_tile_experiment(std::string filename, CoOrdinate l_dims, CoOrdinate r
 
 
    double elapsed = time_span.count();
-   write(pipefd[1], &elapsed, sizeof(elapsed));
+   write(pipefd[1], &elapsed, sizeof(double));
    close(pipefd[1]);
 
    exit(0);
@@ -56,7 +59,7 @@ double run_tile_experiment(std::string filename, CoOrdinate l_dims, CoOrdinate r
    int stat;
    wait(&stat);
    double return_val;
-   read(pipefd[0], &return_val, sizeof(return_val));
+   read(pipefd[0], &return_val, sizeof(double));
    close(pipefd[0]);
    return return_val;
 
@@ -71,8 +74,29 @@ int get_l2_num_elts(){
 }
 
 
+void run_experiment(std::string filename, CoOrdinate contraction_coord, int tile_size){
+
+  double dense_time = run_tile_experiment<TileAccumulator<double>>(filename, contraction_coord, contraction_coord, tile_size, tile_size);
+  double mask_8_time = run_tile_experiment<maskedAccumulator<double, uint8_t>>(filename, contraction_coord, contraction_coord, tile_size, tile_size);
+  double mask_16_time = run_tile_experiment<maskedAccumulator<double, uint16_t>>(filename, contraction_coord, contraction_coord, tile_size, tile_size);
+  double mask_32_time = run_tile_experiment<maskedAccumulator<double, uint32_t>>(filename, contraction_coord, contraction_coord, tile_size, tile_size);
+  double mask_64_time = run_tile_experiment<maskedAccumulator<double, uint64_t>>(filename, contraction_coord, contraction_coord, tile_size, tile_size);
+
+  std::cout << "Times for " << filename << " " << tile_size << std::endl;
+  std::cout << dense_time << " " << mask_8_time << " " << mask_16_time << " " << mask_32_time << " " << mask_64_time << std::endl;
+}
 
 
+
+void run_all_tile_sizes(std::string filename, CoOrdinate contraction_coord){
+
+  run_experiment(filename, contraction_coord, 64);
+  run_experiment(filename, contraction_coord, 128);
+  run_experiment(filename, contraction_coord, 256);
+  run_experiment(filename, contraction_coord, 512);
+  run_experiment(filename, contraction_coord, 1024);
+
+}
 
 int main() {
 
@@ -80,12 +104,18 @@ int main() {
   //std::vector<float> tile_scaling_factors = {0.04, 0.2, 1, 5, 25, 125, 625, 78125, 390625};
   //std::vector<int> tile_scaling_factors = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536};
  
-  std::string filename = "vast-5d.tns";
-  double dense_time = run_tile_experiment<TileAccumulatorDense<double>>(filename, CoOrdinate({0,1}), CoOrdinate({0,1}), 1024, 1024);
-  double mask_8_time = run_tile_experiment<maskedAccumulator<double, uint8_t>>(filename, CoOrdinate({0,1}), CoOrdinate({0,1}), 1024, 1024);
-  double mask_16_time = run_tile_experiment<maskedAccumulator<double, uint16_t>>(filename, CoOrdinate({0,1}), CoOrdinate({0,1}), 1024, 1024);
-  double mask_32_time = run_tile_experiment<maskedAccumulator<double, uint32_t>>(filename, CoOrdinate({0,1}), CoOrdinate({0,1}), 1024, 1024);
-  double mask_64_time = run_tile_experiment<maskedAccumulator<double, uint64_t>>(filename, CoOrdinate({0,1}), CoOrdinate({0,1}), 1024, 1024);
+
+  run_all_tile_sizes("nips.tns", CoOrdinate({0,1,3}));
+
+  run_all_tile_sizes("chicago.tns", CoOrdinate({0}));
+  run_all_tile_sizes("chicago.tns", CoOrdinate({0,1}));
+  run_all_tile_sizes("chicago.tns", CoOrdinate({1,2,3}));
+
+  run_all_tile_sizes("vast-5d.tns", CoOrdinate({0,1}));
+  run_all_tile_sizes("vast-5d.tns", CoOrdinate({0,1,4}));
+
+  run_all_tile_sizes("uber.tns", CoOrdinate({0,2}));
+  run_all_tile_sizes("uber.tns", CoOrdinate({1,2,3}));
 
   // double dense_time = run_tile_experiment<TileAccumulatorDense<double>>(filename, CoOrdinate({0}), CoOrdinate({0}), 128, 128);
   // double mask_8_time = run_tile_experiment<maskedAccumulator<double, uint8_t>>(filename, CoOrdinate({0}), CoOrdinate({0}), 128, 128);

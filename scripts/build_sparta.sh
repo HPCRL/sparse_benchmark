27176d191dc8e6a5eab11d2c889917c97d754066
huntermBerkeley <hunterm@berkeley.edu>
2025-07-15 23:09:26 -0400

    patch for SPARTA

--- scripts/build_sparta.sh
+++ scripts/build_sparta.sh
@@ -5,8 +5,13 @@ export EXPERIMENT_MODES=3
 export TENSOR_DIR=$CURRENT_DIR/fastcc_tensors
 export SPARTA_DIR=$CURRENT_DIR/HiParTI
 
+cp -f patches/ttt.c HiParTI/benchmark/ttt.c
+
 cd HiParTI
-mkdir build -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+mkdir build
 cd build
-cmake ..
+cmake ..  -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+make -j
+cd ..
+cd ..
 
